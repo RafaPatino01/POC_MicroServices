93c1f510e1cba18f2a285401446a82d7771c57dc
Rafael Patino Goji <RafaPatino01@outlook.com>
2023-02-27 15:26:49 -0600

    updated gateway

--- gateway/index.js
+++ gateway/index.js
@@ -5,7 +5,7 @@ const proxy = require("express-http-proxy")
 const app = express()
 
 const PORT = 8000;
-const HOST = 'localhost';
+const HOST = '0.0.0.0';
 
 app.use(cors())
 app.use(express.json())
@@ -15,6 +15,10 @@ app.use(express.json())
 app.use("/customer", proxy("http://110.238.83.6:30001"))
 app.use("/shopping", proxy("http://110.238.83.6:30003"))
 app.use("/", proxy("http://110.238.83.6:30002"))
+
+app.get("/holamundo", (req,res) =>{
+    res.send("hola mundooooo")
+})
 
 app.listen(PORT, HOST, ()=>{
     console.log(`Gateway on http://${HOST}:${PORT}`);
